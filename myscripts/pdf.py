40d599d1372b7f67691f53dc78ee7be55863cb42
ravikiransrinivas <40417316+ravikiransrinivas@users.noreply.github.com>
2018-06-20 11:30:12 +0530

    Add files via upload

---
+++ myscripts/pdf.py
@@ -0,0 +1,5 @@
+import subprocess
+import shlex
+
+subprocess.call(shlex.split(
+    '/home/vignesh/Users/me/myscripts/extract_text.sh /home/vignesh/Users/me/mypdfs/ /home/vignesh/Users/me/myplaintext/'))
